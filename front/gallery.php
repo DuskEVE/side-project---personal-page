559fc166a2ae9a3778a34fa9828c8bdfb01ddb72
DuskEVE <x987000@gmail.com>
2024-01-14 17:13:59 +0800

    finish gallery front page prototype

--- front/gallery.php
+++ front/gallery.php
@@ -8,12 +8,21 @@
         </div>
     </div>
     <div class="col-12 col-md-3 gallery-grid">
-        <img class="gallery-img" src="https://picsum.photos/id/2/600/400" alt="">
+        <img class="gallery-img" src="https://picsum.photos/id/2/400/400" alt="">
+        <div class="gallery-title">
+            <span>test img 02</span>
+        </div>
     </div>
     <div class="col-12 col-md-3 gallery-grid">
         <img class="gallery-img" src="https://picsum.photos/id/3/600/400" alt="">
+        <div class="gallery-title">
+            <span>test img 03</span>
+        </div>
     </div>
     <div class="col-12 col-md-3 gallery-grid">
         <img class="gallery-img" src="https://picsum.photos/id/4/600/400" alt="">
+        <div class="gallery-title">
+            <span>test img 04</span>
+        </div>
     </div>
 </div>
