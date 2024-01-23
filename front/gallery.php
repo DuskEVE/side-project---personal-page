25059bd74aede90e6b7aa2130708b5866adb415a
DuskEVE <x987000@gmail.com>
2024-01-23 15:24:39 +0800

    gallery now can display order by update time and like count

--- front/gallery.php
+++ front/gallery.php
@@ -4,20 +4,36 @@ if(isset($_GET['type'])) $total = $Gallery->count(['type_id'=>$_GET['type']]);
 else if(isset($_GET['user'])) $total = $Gallery->count(['user'=>$_GET['user']]);
 else $total = $Gallery->count();
 $pageCount = ceil($total / 20);
-$currentPage = (isset($_GET['p'])? $_GET['p']:1);
+$currentPage = (isset($_GET['p']) && $_GET['p']<=$pageCount? $_GET['p']:1);
 $start = ($currentPage-1) * 20;
 $end = ($currentPage==$pageCount? $total:$currentPage*20);
+
+function getUrl($get){
+    $targetSet = [];
+    foreach($get as $key=>$value){
+        array_push($targetSet, "$key=$value");
+    }
+    return "./index.php?".(join("&", $targetSet));
+}
+
+$orderDate = $_GET;
+$orderDate['order'] = "id";
+$orderLike = $_GET;
+$orderLike['order'] = "like_count";
 ?>
 
 <div class="container">
     <div class="d-flex justify-content-center">
-        <button class="btn btn-warning m-3">最新上傳</button>
-        <button class="btn btn-secondary m-3">最多人喜歡</button>
+        <a class="btn btn-warning m-3" href="<?=getUrl($orderDate)?>">最新上傳</a>
+        <a class="btn btn-secondary m-3" href="<?=getUrl($orderLike)?>">最多人喜歡</a>
     </div>
 
     <?php
     $gallerys;
-    $option = "order by `id` desc limit $start,20";
+    $order = "id";
+    if(isset($_GET['order']) && in_array($_GET['order'], ["id", "like_count"])) $order = $_GET['order'];
+    $option = "order by `$order` desc limit $start,20";
+
     if(isset($_GET['type'])) $gallerys = $Gallery->searchAll(['type_id'=>$_GET['type']], $option);
     else if(isset($_GET['user'])) $gallerys = $Gallery->searchAll(['user'=>$_GET['user']], $option);
     else $gallerys = $Gallery->searchAll([], $option);
@@ -62,7 +78,9 @@ $end = ($currentPage==$pageCount? $total:$currentPage*20);
             if(isset($_GET['type'])) $target = "&type={$_GET['type']}";
             else if(isset($_GET['user'])) $target = "&user={$_GET['user']}";
             $btn = ($i==$currentPage? "btn-primary":"btn-secondary");
-            echo "<a class='btn $btn ms-2 me-2' href='./index.php?do=gallery$target&p=$i'>$i</a>";
+            $page = $_GET;
+            $page['p'] = $i;
+            echo "<a class='btn $btn ms-2 me-2' href='".(getUrl($page))."'>$i</a>";
         }
     }
     ?>
