4c598ea67e2d3e295739f21c3c46439911f8e386
DuskEVE <x987000@gmail.com>
2024-01-29 11:39:08 +0800

    finish basic layout of news page

--- front/news.php
+++ front/news.php
@@ -1,56 +1,41 @@
 
 <div style="width: 100%; height: 50px;"></div>
 
-<div class="container text-light d-flex flex-column align-items-center">
+<div class="container text-light">
+    <div class="d-flex justify-content-around flex-wrap dusk-bg-gray">
+        <a class="btn btn-warning m-3" href="">最新消息</a>
+        <?php
+        $types = $Type->searchAll(['display'=>1]);
+        foreach($types as $type){
+            echo "<a class='btn btn-secondary m-3' href=''>{$type['name']}</a>";
+        }
+        ?>
+    </div>
 
     <?php
-    $newsList = $News->searchAll(['display'=>1], "order by `id` desc limit 10");
-    foreach($newsList as $index=>$news){
-        $content = nl2br($news['text']);
-        echo "
-        <div class='mb-3 p-3 dusk-bg-gray news-preview col-12 col-md-6'>
-            <h3 class='text-center'>{$news['title']}</h3>
-            <div class='d-flex justify-content-center'>
-                <img src=''>
-            </div>
-            <p>$content</p>
-        </div>
-        ";
+    $newsList = $News->searchAll(['display'=>1], "order by `id` desc limit 6");
+    $index = 0;
+    for($i=0; $i<3; $i++){
+        echo "<div class='row'>";
+        for($j=0; $j<2; $j++){
+            $news = $newsList[$index];
+            $index++;
+            $content = nl2br($news['text']);
+            echo "
+            <div class='mb-3 p-2 col-12 col-md-6'>
+                <div class='dusk-bg-gray news-preview p-2'>
+                    <h3 class='text-center'>{$news['title']}</h3>
+                    <div class='d-flex justify-content-center'>
+                        <img src=''>
+                    </div>
+                    <p>$content</p>
+                </div>
+            </div>";
+            if($index+1 > count($newsList)) break;
+        }
+        echo "</div>";
+        if($index+1 > count($newsList)) break;
     }
     ?>
-
-    <!-- <div class="mb-3 p-3 dusk-bg-gray news-preview">
-        <div class="d-flex justify-content-center">
-            <img src="https://picsum.photos/id/1/300/300">
-        </div>
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Fuga, veniam aut deleniti eos voluptatum, 
-            nostrum temporibus voluptates nam vel, 
-            quas rem voluptas id similique. Omnis voluptas fugit saepe obcaecati necessitatibus?
-        </p>
-    </div>
-    <div class="mb-3 p-3 dusk-bg-gray news-preview">
-        <div class="d-flex justify-content-center">
-            <img src="https://picsum.photos/id/2/300/300">
-        </div>
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Fuga, veniam aut deleniti eos voluptatum, 
-            nostrum temporibus voluptates nam vel, 
-            quas rem voluptas id similique. Omnis voluptas fugit saepe obcaecati necessitatibus?
-        </p>
-    </div>
-    <div class="mb-3 p-3 dusk-bg-gray news-preview">
-        <div class="d-flex justify-content-center">
-            <img src="https://picsum.photos/id/3/300/300">
-        </div>
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Fuga, veniam aut deleniti eos voluptatum, 
-            nostrum temporibus voluptates nam vel, 
-            quas rem voluptas id similique. Omnis voluptas fugit saepe obcaecati necessitatibus?
-        </p>
-    </div> -->
 
 </div>
