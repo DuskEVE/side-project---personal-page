d91ba709248f068c906963460fea4056710e36d4
DuskEVE <x987000@gmail.com>
2024-03-20 19:21:42 +0800

    hash user password

--- api/login_check.php
+++ api/login_check.php
@@ -2,14 +2,17 @@
 include_once "./db.php";
 
 if(isset($_POST['user']) && isset($_POST['password'])){
-    $check = $User->count($_POST);
+    $check = $User->count(['user'=>$_POST['user']]);
     if($check){
-        $_SESSION['user'] = $_POST['user'];
-        $data = $User->search($_POST);
-        if($data['admin'] == 1) $_SESSION['admin'] = 1;
-        echo "success";
+        $user = $User->search(['user'=>$_POST['user']]);
+        if(password_verify($_POST['password'], $user['password'])){
+            $_SESSION['user'] = $_POST['user'];
+            if($user['admin'] == 1) $_SESSION['admin'] = 1;
+            echo "success";
+        }
+        else echo "密碼錯誤!";
     }
-    else echo "帳號或密碼錯誤!";
+    else echo "此帳號不存在!";
 }
 else if(!isset($_POST['user'])) echo "請輸入使用者帳號!";
 else if(!isset($_POST['password'])) echo "請輸入使用者密碼!";
--- api/reg.php
+++ api/reg.php
@@ -5,6 +5,7 @@ $check = $User->count(['user'=>$_POST['user']]);
 if($check) echo "該帳號已被註冊!";
 else{
     $_POST['admin'] = 0;
+    $_POST['password'] = password_hash($_POST['password'], PASSWORD_DEFAULT);
     $User->update($_POST);
     $_SESSION['user'] = $_POST['user'];
     echo "success";
