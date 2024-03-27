908d124303afe72198674e8fa993c48e68708724
DuskEVE <x987000@gmail.com>
2024-03-27 19:31:31 +0800

    add comment in db.php

--- api/db.php
+++ api/db.php
@@ -16,7 +16,7 @@ class myDB{
         $this->user = $user;
         $this->password = $password;
     }
-
+    // 建構新的PDO物件來和資料庫互動
     function dbLogIn(){
         $dns = "mysql:host=$this->host;charset=$this->charset;dbname=$this->database";
         return new PDO($dns, $this->user, $this->password);
@@ -34,7 +34,7 @@ class myDB{
     function switchTable($table){
         $this->table = $table;
     }
-
+    // 將目標關聯陣列轉換成符合SQL搜尋語法的字串
     function targetToString($target, $separator){
         $targetSet = [];
         foreach($target as $key=>$value){
@@ -43,12 +43,12 @@ class myDB{
 
         return implode($separator, $targetSet);
     }
-
+    // 直接執行完整的sql語法
     function sql($sql){
         $pdo = $this->dbLogIn();
         return $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
     }
-
+    // 搜尋並回傳符合條件的單筆資料
     function search($target){
         $pdo = $this->dbLogIn();
         $targetSet = $this->targetToString($target, "&&");
@@ -56,7 +56,7 @@ class myDB{
 
         return $pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
     }
-
+    // 搜尋並回傳符合條件的所有資料
     function searchAll($target=[], $option=""){
         $pdo = $this->dbLogIn();
         $sql = "select * from `$this->table`";
@@ -68,7 +68,7 @@ class myDB{
 
         return $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
     }
-
+    // 新增或更新現有資料(透過輸入的陣列是否包含目標資料的id來辨別)
     function update($target){
         $pdo = $this->dbLogIn();
         $sql = "";
@@ -87,7 +87,7 @@ class myDB{
 
         return $pdo->exec($sql);
     }
-
+    // 刪除單筆指定的資料
     function delete($target){
         $pdo = $this->dbLogIn();
         $targetSet = $this->targetToString($target, "&&");
@@ -95,7 +95,7 @@ class myDB{
 
         return $pdo->exec($sql);
     }
-
+    // 回傳符合搜尋條件的資料筆數
     function count($target=[]){
         $pdo = $this->dbLogIn();
         $sql = "select count(*) from `$this->table`";
@@ -108,7 +108,7 @@ class myDB{
         return $count['count(*)'];
     }
 }
-
+// 宣告操作所有資料表所需的myDB物件
 $User = new myDB('localhost', 'utf8', 'p01', 'root', '', 'p01_user');
 $Banner = new myDB('localhost', 'utf8', 'p01', 'root', '', 'p01_banner');
 $Type = new myDB('localhost', 'utf8', 'p01', 'root', '', 'p01_type');
